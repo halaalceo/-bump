d9cf0c4ba5fc44422c0f11ecd30a8b71dfc3fd57
halaalceo <166223277+halaalceo@users.noreply.github.com>
2024-04-25 20:18:24 +0500

    config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 ### config file ###
-token = "" # Put your discord token here.
+token = "MTIxODQ4ODUwMDE3Nzg2Njc5Mg.GszU4J.ZuuLpNXzjwZb6DDRJZ89NtRBE6Jlhkfm-9trQQ" # Put your discord token here.
 preifx = ";" # Put the prefix you want here.
 wait_time = "7460" # Put the wait time in seconds. !!!Dont change this unless you know what you're doing.!!!
-disboard_prefix = "!d" # Put the disboard prefix here no spaces.
+disboard_prefix = "/" # Put the disboard prefix here no spaces.
 stream = True # Have a streaming status to my youtube video.
